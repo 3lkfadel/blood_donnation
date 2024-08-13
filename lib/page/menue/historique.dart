93e7e43f53a05d3afb04246553a4239f8187af11
3lkfadel <3lkfadel@gmail.com>
2024-08-13 14:10:59 +0000

    historique

--- lib/page/menue/historique.dart
+++ lib/page/menue/historique.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class Historique extends StatefulWidget {
@@ -9,11 +7,82 @@ class Historique extends StatefulWidget {
   State<Historique> createState() => _HistoriqueState();
 }
 
-class _HistoriqueState extends State<Historique> {
+class _HistoriqueState extends State<Historique> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Placeholder(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'HISTO',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'TRIQUE',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: 'Don effectué'),
+            Tab(text: 'Demande effectuée'),
+          ],
+        ),
+       
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                
+              ],
+            ),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+               
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
