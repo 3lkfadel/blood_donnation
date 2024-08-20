b18164f1796eec21bdaf31a598dc2a901334e5c7
3lkfadel <3lkfadel@gmail.com>
2024-08-20 11:23:11 +0000

    maps up

--- lib/page/map/map.dart
+++ lib/page/map/map.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -10,8 +11,6 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   static const LatLng _pGooglePlex = LatLng(37.4223, -122.0848);
-
-  // Ajout d'un marqueur
   final Set<Marker> _markers = {
     Marker(
       markerId: MarkerId('_pGooglePlex'),
@@ -21,13 +20,33 @@ class _MapScreenState extends State<MapScreen> {
   };
 
   @override
+  void initState() {
+    super.initState();
+    _requestLocationPermission();
+  }
+
+  Future<void> _requestLocationPermission() async {
+    var status = await Permission.locationWhenInUse.status;
+    if (status.isDenied) {
+      if (await Permission.locationWhenInUse.request().isGranted) {
+        // Permission granted
+      } else {
+        // Permission denied
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('Carte avec Points')),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
           target: _pGooglePlex,
           zoom: 13,
         ),
+        markers: _markers,
+        myLocationEnabled: true, // Afficher la position actuelle de l'utilisateur si permission accordée
       ),
     );
   }
