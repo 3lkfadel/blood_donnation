724451b43820a913b2192a817cfa202c37fbd66e
3lkfadel <3lkfadel@gmail.com>
2024-08-24 10:50:29 +0000

    upgrade_home_bar

--- lib/page/root/navroot.dart
+++ lib/page/root/navroot.dart
@@ -4,8 +4,6 @@ import 'package:blood_donnation/page/menue/AnnoncePage.dart';
 import 'package:blood_donnation/page/menue/SettingPage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-
 class Navroot extends StatefulWidget {
   const Navroot({super.key});
 
@@ -14,72 +12,74 @@ class Navroot extends StatefulWidget {
 }
 
 class _NavrootState extends State<Navroot> {
-   int _selectIndex = 0;
-   final _screens =  [
+  int _selectIndex = 0;
+  final _screens = [
     HomePage(),
     // FeedPage(),
     Annoncepage(),
     SettingPage(),
-
-
   ];
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       body: _screens[_selectIndex],
-     bottomNavigationBar: Container(
-  decoration: BoxDecoration(
-    boxShadow: [
-      BoxShadow(  // effet d'ombre 
-        color: Colors.grey.withOpacity(0.5),
-        spreadRadius: 1,
-        blurRadius: 7,
-        offset: Offset(0, -3), // direction de l'ombre 
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20), // Coins arrondis
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 10,
+                offset: Offset(0, 4), // Ombre vers le bas
+              ),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20), // Assure que la barre de navigation respecte les coins arrondis
+            child: BottomNavigationBar(
+              backgroundColor: Colors.transparent, // Fond transparent pour montrer le Container parent
+              elevation: 0, // Supprime l'élévation par défaut
+              type: BottomNavigationBarType.fixed,
+              selectedItemColor: Color(0xFF7165D6),
+              unselectedItemColor: Colors.black,
+              selectedLabelStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+              ),
+              currentIndex: _selectIndex,
+              onTap: (index) {
+                setState(() {
+                  _selectIndex = index;
+                });
+              },
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home_filled),
+                  label: "",
+                ),
+                // BottomNavigationBarItem(
+                //   icon: Icon(CupertinoIcons.chat_bubble),
+                //   label: "",
+                // ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.calendar_month),
+                  label: "",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.settings),
+                  label: "",
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-    ],
-  ),
-  child: ClipRRect(
-    borderRadius: BorderRadius.only( 
-      topLeft: Radius.circular(20), 
-      topRight: Radius.circular(20), 
-       ),
-       child: BottomNavigationBar(
-      backgroundColor: Colors.white,
-      type: BottomNavigationBarType.fixed,
-      selectedItemColor: Color(0xFF7165D6),
-      unselectedItemColor: Colors.black,
-      selectedLabelStyle: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 15,
-      ),
-      currentIndex: _selectIndex,
-      onTap: (index) {
-        setState(() {
-          _selectIndex = index;
-        });
-      },
-      items: [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home_filled),
-          label: "",
-        ),
-        // BottomNavigationBarItem(
-        //   icon: Icon(CupertinoIcons.chat_bubble),
-        //   label: "",
-        // ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.calendar_month),
-          label: "",
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.settings),
-          label: "",
-        ),
-      ],
-    ),
-  ),
-     ),
     );
   }
 }
