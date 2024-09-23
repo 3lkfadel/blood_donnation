a2eb5d06a2a723aa8e5a88acf1b08ca56fbaef45
Cheich-Wendyam <cheich.yalaweogo61@gmail.com>
2024-09-23 11:30:55 +0000

    correcte

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,9 +1,10 @@
 
 class ApiEndpoints {
-  static const String baseUrl = 'http://192.168.1.68:8000/api';
+  static const String baseUrl = 'http://192.168.11.114:8000/api';
   static const String register = '$baseUrl/register';
   static const String login = '$baseUrl/login';
   static const String profile = '$baseUrl/profile';
+  static const String logout = '$baseUrl/logout';
   static const String updateProfile = '$baseUrl/updateprofile';
   static const String updateBloodGroup = '$baseUrl/groupsanguin';
   static const String annonces ='$baseUrl/annonces';
@@ -12,7 +13,7 @@ class ApiEndpoints {
   static const String mark = '$baseUrl/mark';
   static const String history = '$baseUrl/historiques';
   static const String centre = '$baseUrl/centres';
-  static const String imageurl = 'http://192.168.1.68:8000/storage/';
+  static const String imageurl = 'http://192.168.11.114:8000/storage/';
   static const String don = '$baseUrl/dons';
   static const String verify = '$baseUrl/verify';
 
